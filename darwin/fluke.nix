c423f94d4fe1da6fa23ce8f4fda3055d7b3d1c06
Beau Simensen <beau@dflydev.com>
2024-01-11 12:02:46 -0600

    Add FlakeHub CLI, remove neovim theme

--- darwin/fluke.nix
+++ darwin/fluke.nix
@@ -41,6 +41,7 @@
       #git
       pfetch
       #ranger
+      fh
 
       # Doom Emacs
       #emacs
@@ -649,26 +650,22 @@
           vim-gitgutter                   # See uncommitted changes of file :GitGutterEnable
 
           # File Tree
-          nerdtree                        # File Manager - set in extraConfig to F6
+          #nerdtree                        # File Manager - set in extraConfig to F6
 
           # Customization
           wombat256-vim                   # Color scheme for lightline
-          srcery-vim                      # Color scheme for text
+          #srcery-vim                      # Color scheme for text
 
           lightline-vim                   # Info bar at bottom
-          indent-blankline-nvim           # Indentation lines
+          #indent-blankline-nvim           # Indentation lines
         ];
 
         extraConfig = ''
           syntax enable                             " Syntax highlighting
-          colorscheme srcery                        " Color scheme text
 
           let g:lightline = {
             \ 'colorscheme': 'wombat',
             \ }                                     " Color scheme lightline
-
-          highlight Comment cterm=italic gui=italic " Comments become italic
-          hi Normal guibg=NONE ctermbg=NONE         " Remove background, better for personal theme
 
           set number                                " Set numbers
 
